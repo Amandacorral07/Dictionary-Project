2b9abf0ea338a8260eec53cdc2e7e034bb24c654
Amandacorral07 <amandalewis0707@gmail.com>
2022-06-01 11:06:17 -0700

    Set up Search Engine in Dictionary.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
+    <div className="container">
     <div className="App">
       <header className="App-header">
         Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by Amanda Corral
+      </footer>
+    </div>
     </div>
   );
 }
