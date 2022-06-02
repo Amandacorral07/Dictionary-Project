511318d80c1712a72e9d8a6fbd1d68716d475789
Amandacorral07 <amandalewis0707@gmail.com>
2022-06-02 12:10:09 -0700

    Imported Axios, rendered the word meanings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './App.css';
 import Dictionary from "./Dictionary";
-import axios from "axios";
+
 
 function App() {
   return (
     <div className="container">
     <div className="App">
       <header className="App-header">
-        Hello
+        
       </header>
       <main>
         <Dictionary />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import Results from "./Results.js"
 import axios from "axios";
 
 export default function Dictionary (){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
 function handleResponse(response){
-    return (response.data[0]);
+    console.log (response.data[0]);
+    setResults(response.data[0]);
 }
 
     function search (event){
@@ -21,11 +24,14 @@ axios.get(apiUrl).then(handleResponse);
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
+
+
     return (
     <div className="Dictionary">
 <form onSubmit={search}>
     <input type="search" onChange={handleKeywordChange}/>
 </form>
+<Results results={results}/>
         </div>
     );
 }
