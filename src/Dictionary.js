e1c4444cfe26e0c2cd47be91989d85c998d37c5d
Amandacorral07 <amandalewis0707@gmail.com>
2022-06-04 16:27:46 -0700

    Added Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,33 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import Results from "./Results.js"
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary (props){
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded]= useState(false);
+    let [photos, setPhotos]= useState(null);
 
 function handleResponse(response){
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos);
 }
 
 function search(){
    //documentation: https://dictionaryapi.dev/
    let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
    axios.get(apiUrl).then(handleResponse);
+
+   let pexelsApiKey =  `563492ad6f91700001000001cb4c68f8ab7d4251beca166740ddd883`;
+   let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+   let headers ={"Authorization" : `Bearer ${pexelsApiKey}`};
+
+axios.get(pexelsApiUrl, {headers: headers} ).then(handlePexelsResponse);
 }
 
 
@@ -47,6 +59,7 @@ if (loaded){
 </div>
 </section>
 <Results results={results}/>
+<Photos photos={photos}/>
         </div>
     );
     } else {
